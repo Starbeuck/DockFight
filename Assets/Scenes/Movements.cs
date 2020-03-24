2faf8a687e7de6121f015fda448392d8862e3ef2
Kanaillou <quentin.daumand@gmail.com>
2020-03-24 23:38:13 +0100

    Added a test terrain + script for make the ship move = pitch + roll  . Added a perpetual and linear forward movement

---
+++ Assets/Scenes/Movements.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movements : MonoBehaviour
+{
+  
+    void Start() { }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward * Time.deltaTime * 90.0f;
+        transform.Rotate(Input.GetAxis("Vertical"), 0.0f, -Input.GetAxis("Horizontal"));   
+    }
+}
